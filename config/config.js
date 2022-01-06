f3a9cd32e1b0a4b6319cfcad4809677c74302f9c
Pce-iyos <61274755+Pce-iyos@users.noreply.github.com>
2022-01-06 11:03:11 +0100

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  // HOST: "mysql-peace.alwaysdata.net",
+  // USER: "peace",
+  // PASSWORD: "Osamuyi4real",
+  // DB: "peace_olap_system",
+  
+  development: {
+    username: "peace",
+    password: "Osamuyi4real",
+    database: "peace_olap_system",
+    host: "mysql-peace.alwaysdata.net",
+    dialect: "mysql",
+  },
+  test: {
+    username: "peace",
+    password: "Osamuyi4real",
+    database: "peace_olap_system",
+    host: "mysql-peace.alwaysdata.net",
+    dialect: "mysql",
+  },
+  production: {
+    username: "peace",
+    password: "Osamuyi4real",
+    database: "peace_olap_system",
+    host: "mysql-peace.alwaysdata.net",
+    dialect: "mysql",
+  },
+};
